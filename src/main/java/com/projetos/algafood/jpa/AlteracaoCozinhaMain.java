c6691596e4ad51ac2f3b3128fc2e3d0a07d1b3bc
Zilli <douglaszilli@gmail.com>
2020-01-02 15:22:35 -0300

    Aula 3.12

--- src/main/java/com/projetos/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/projetos/algafood/jpa/AlteracaoCozinhaMain.java
@@ -6,7 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -15,16 +15,12 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
+        cozinha = cadastroCozinha.salvar(cozinha);
 
-        cozinha1 = cadastroCozinha.adicionar(cozinha1);
-        cozinha2 = cadastroCozinha.adicionar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+        System.out.printf("%d - %s\n", cozinha.getId(), cozinha.getNome());
     }
 }
