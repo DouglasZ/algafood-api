1c01a2c5d4e9aa5f75196441eb231911ed1eae42
Zilli <douglaszilli@gmail.com>
2020-01-02 17:00:21 -0300

    Aula 3.15

--- src/main/java/com/projetos/algafood/domain/model/Cozinha.java
+++ src/main/java/com/projetos/algafood/domain/model/Cozinha.java
@@ -1,51 +1,22 @@
 package com.projetos.algafood.domain.model;
 
-import java.util.Objects;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Cozinha {
+public class Cozinha
+{
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String nome;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return id.equals(cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public void setId( Long id )
-    {
-        this.id = id;
-    }
-
-    public String getNome()
-    {
-        return nome;
-    }
-
-    public void setNome( String nome )
-    {
-        this.nome = nome;
-    }
+	private String nome;
 }
