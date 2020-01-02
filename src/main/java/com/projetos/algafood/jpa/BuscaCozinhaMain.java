ac7c00559e805dd3205088c3f182f5e7e7118398
Zilli <douglaszilli@gmail.com>
2020-01-02 15:04:43 -0300

    Aula 3.10

--- src/main/java/com/projetos/algafood/jpa/BuscaCozinhaMain.java
+++ src/main/java/com/projetos/algafood/jpa/BuscaCozinhaMain.java
@@ -6,7 +6,9 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+import java.util.List;
+
+public class BuscaCozinhaMain {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
@@ -15,16 +17,8 @@ public class InclusaoCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
-
-        cozinha1 = cadastroCozinha.adicionar(cozinha1);
-        cozinha2 = cadastroCozinha.adicionar(cozinha2);
-
-        System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+        System.out.println(cozinha.getNome());
     }
 }
