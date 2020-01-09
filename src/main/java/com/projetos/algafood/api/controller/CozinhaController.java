1ed11610bfa4b3c19d816bcb1e0fec7d5e62162c
Zilli <douglaszilli@gmail.com>
2020-01-09 20:50:37 -0300

    Aula 4.20

--- src/main/java/com/projetos/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/projetos/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,12 +30,13 @@ public class CozinhaController
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXmlWrapper listarXml()
 	{
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+		return new CozinhasXmlWrapper( cozinhaRepository.listar() );
 	}
 
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId)
+	public ResponseEntity<Cozinha> buscar( @PathVariable Long cozinhaId )
 	{
-		return cozinhaRepository.buscar(cozinhaId);
+		Cozinha cozinha = cozinhaRepository.buscar( cozinhaId );
+		return ResponseEntity.ok( cozinha );
 	}
 }
