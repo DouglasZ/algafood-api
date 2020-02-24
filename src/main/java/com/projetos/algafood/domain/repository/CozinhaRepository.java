f229b5b38d61a413c1c14a0f35bb95b2e8ca06e1
Zilli <douglaszilli@gmail.com>
2020-02-24 11:49:37 -0300

    Aula 5.6

--- src/main/java/com/projetos/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/projetos/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,7 @@
 package com.projetos.algafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.projetos.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>
 {
-//	List<Cozinha> consultarPorNome( String Nome );
+	List<Cozinha> findByNome( String Nome );
 }
